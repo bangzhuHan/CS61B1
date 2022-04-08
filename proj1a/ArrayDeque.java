abb09c7ae6a2da4b36280f8dee14d62947b06791
岑溪 <2365971195@qq.com>
2022-04-08 18:38:48 +0800

    Project 1A

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -18,233 +18,323 @@
  * 您的使用系数应始终至少为25%。对于较小的阵列，您的使用系数可以任意低。
  */
 
+//public class ArrayDeque<T> {
+//    private int size = 0;
+//
+//    private int nextFirst;
+//    private int nextLast;
+//
+//    private int item = 0;
+//
+//    private T[] arrays;
+//
+//
+//    /**
+//     * Creates an empty linked list deque.
+//     */
+//    public ArrayDeque() {
+//        arrays = (T[]) new Object[8];
+//        this.nextFirst = item + 1;
+//        this.nextLast = item + 2;
+//    }
+//
+//    /**
+//     * Adds an item of type T to the front of the deque
+//     * @param t
+//     */
+//    public void addFirst(T t){
+//        if(this.size == this.arrays.length - 2){
+//            resizeWithIncrease();
+//        }
+//
+//        this.arrays[this.nextFirst] = t;
+//
+//        if(this.nextFirst == 0){
+//            this.nextFirst = this.arrays.length - 1;
+//        } else {
+//            this.nextFirst --;
+//        }
+//        size++;
+//    }
+//
+//    /**
+//     * Adds an item of type T to the back of the deque
+//     * @param t
+//     */
+//    public void addLast(T t){
+//        if(this.size == this.arrays.length - 2){
+//            resizeWithIncrease();
+//        }
+//
+//        this.arrays[this.nextLast] = t;
+//        if(this.nextLast == this.arrays.length - 1){
+//            this.nextLast = 0;
+//        }else {
+//            this.nextLast++;
+//        }
+//        size++;
+//    }
+//
+//    /**
+//     * Returns true if deque is empty, false otherwise.
+//     * @return
+//     */
+//    public boolean isEmpty(){
+//        //return this.nextLast - this.nextFirst == 1;
+//        if (size == 0) {
+//            return true;
+//        }
+//
+//        return false;
+//    }
+//
+//    /**
+//     * Returns the number of items in the deque.
+//     * @return
+//     */
+//    public int size() {
+//        return size;
+//    }
+//
+//    /**
+//     * Prints the items in the deque from first to last, separated by a space.
+//     */
+//    public void printDeque(){
+////        for(int i = this.nextFirst + 1; i < size; i++){
+////            System.out.print(this.arrays[i] + " ");
+////        }
+////        System.out.println();
+//        boolean flag = true;
+//        int first = this.nextFirst + 1;
+//        int last = this.nextLast;
+//
+//        while (flag) {
+//
+//            System.out.print(this.arrays[first]);
+//            first++;
+//
+//            if (first > this.arrays.length - 1) {
+//                first = 0;
+//            }
+//
+//            if (first == last) {
+//                flag = false;
+//            }
+//        }
+//
+//        System.out.println();
+//
+//    }
+//
+//    private void resizeWithIncrease() {
+//        //从nextFirst -> nextLast
+//        T[] newArray = (T[]) new Object[this.arrays.length * 2];
+//        boolean flag = true;
+//        int index = 0;
+//
+//        while(flag){
+//            //结点相邻
+//            if(this.nextFirst != this.nextLast){
+//                //这里还有个特殊结点,就是nextFirst在尾部结点
+//                if(this.nextFirst == this.arrays.length - 1){
+//                    //nextFirst走到了右边界，置于0
+//                    this.nextFirst = 0;
+//                }else {
+//                    this.nextFirst++;
+//                }
+//                //先+1是因为0位留给nextFirst,因为nextFirst要是空位
+//                if(this.arrays[this.nextFirst] != null){
+//                    index++; //从1开始把0留给nextFirst
+//                    newArray[index] = this.arrays[this.nextFirst];
+//                }
+//            } else {
+//                flag = false;
+//            }
+//        }
+//        this.nextFirst = 0;
+//        this.nextLast = index + 1; //最后再加一是因为nextLast要是空的
+//        this.arrays = newArray;
+//    }
+//
+//    private void resizeIDecrease() {
+//        if (this.arrays.length < 16) {
+//            return;
+//        }
+//
+//        if (size * 2 > this.arrays.length) {
+//            return;
+//        }
+//
+//        //空置率至少一半，缩小25%
+//        int length = Double.valueOf(this.arrays.length * 0.75).intValue();
+//        T[] newArray = (T[]) new Object[length];
+//
+//        boolean flag = true;
+//        int index = 0;
+//
+//        while (flag) {
+//            if (this.nextFirst != this.nextFirst) {
+//
+//                if (this.nextFirst >= this.arrays.length - 1) this.nextFirst = 0;
+//                else this.nextFirst++;
+//                if (this.arrays[this.nextFirst] != null) {
+//                    index++;
+//                    newArray[index] = this.arrays[this.nextFirst];
+//                }
+//            }
+//            else flag =false;
+//        }
+//
+//        this.nextFirst = 0;
+//        this.nextLast = index + 1;
+//        this.arrays = newArray;
+//    }
+//
+//    /**
+//     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
+//     * @return
+//     */
+//    public T removeFirst(){
+//        if(isEmpty()) {
+//            return null;
+//        }
+//        if(this.nextFirst == this.arrays.length - 1){
+//            this.nextFirst = 0;
+//        } else {
+//            this.nextFirst ++;
+//        }
+//        T t = this.arrays[this.nextFirst];
+//        this.arrays[this.nextFirst] = null;
+//        size--;
+//        resizeIDecrease();
+//        return t;
+//    }
+//
+//    /**
+//     * Removes and returns the item at the end of the deque. If no such item exists, returns null.
+//     * @return
+//     */
+//    public T removeLast(){
+//        if(isEmpty())
+//            return null;
+//        if(this.nextLast == 0) {
+//            this.nextLast = this.arrays.length - 1;
+//        }
+//        else {
+//            this.nextLast--;
+//        }
+//        T t = this.arrays[this.nextLast];
+//        this.arrays[this.nextLast] = null;
+//        size--;
+//        resizeIDecrease();
+//        return t;
+//    }
+//
+//    /**
+//     *  Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+//     *  If no such item exists, returns null. Must not alter the deque!
+//     * @param index
+//     * @return
+//     */
+//    public T get(int index){
+//        if(index < 0 || index > size - 1)
+//            return null;
+//        int n = this.nextFirst + 1 + index; //n为index位置上的元素在数组中的实际位置
+//
+//        if(n < this.arrays.length)
+//            return this.arrays[n];
+//            else return this.arrays[n - this.arrays.length];
+//    }
+//
+//
+//}
 public class ArrayDeque<T> {
-    private int size = 0;
 
     private int nextFirst;
     private int nextLast;
-
-    private int item = 0;
-
-    private T[] arrays;
-
-
-    /**
-     * Creates an empty linked list deque.
-     */
-    public ArrayDeque() {
-        arrays = (T[]) new Object[8];
-        this.nextFirst = item + 1;
-        this.nextLast = item + 2;
+    private int capacity;
+    private T[]items;
+    private int size;
+    public ArrayDeque(){
+        items=(T[])new Object[8];
+        this.capacity=items.length;
+        nextFirst=capacity-1;
+        nextLast=0;
+        size=0;
     }
-
-    /**
-     * Adds an item of type T to the front of the deque
-     * @param t
-     */
-    public void addFirst(T t){
-        if(this.size == this.arrays.length - 2){
-            resizeWithIncrease();
-        }
-
-        this.arrays[this.nextFirst] = t;
-
-        if(this.nextFirst == 0){
-            this.nextFirst = this.arrays.length - 1;
-        } else {
-            this.nextFirst --;
-        }
+    private void resize(int capacity){
+        T[]a=(T[])new Object[capacity];
+        //由于nextFirst和nextLast的位置不确定，只能一个一个地复制到新的数组中
+        //从nextFirst右边的第一个点开始复制
+        //到nextLast左边的第一个点复制结束
+        for (int i=1;i<=size;i++)
+            a[i]=items[(++nextFirst)%this.capacity];
+        this.capacity=capacity;
+        //这两个指针指向什么地方已经不重要了
+        nextFirst=0;
+        nextLast=size+1;
+        items=a;
+    }
+    public void addFirst(T item) {
+        //直接当size等于capacity时调整大小，而不是看两个指针的相对位置
+        if (size==capacity)
+            resize(capacity*2);
+        items[nextFirst]=item;
         size++;
+        //nextFirst有可能越界
+        nextFirst=nextFirst==0?capacity-1:nextFirst-1;
     }
 
-    /**
-     * Adds an item of type T to the back of the deque
-     * @param t
-     */
-    public void addLast(T t){
-        if(this.size == this.arrays.length - 2){
-            resizeWithIncrease();
-        }
-
-        this.arrays[this.nextLast] = t;
-        if(this.nextLast == this.arrays.length - 1){
-            this.nextLast = 0;
-        }else {
-            this.nextLast++;
-        }
+    public void addLast(T item) {
+        if (size==capacity)
+            resize(capacity*2);
+        items[nextLast]=item;
         size++;
+        //nextLast有可能越界
+        nextLast=(nextLast+1)%capacity;
     }
 
-    /**
-     * Returns true if deque is empty, false otherwise.
-     * @return
-     */
-    public boolean isEmpty(){
-        //return this.nextLast - this.nextFirst == 1;
-        if (size == 0) {
-            return true;
-        }
-
-        return false;
+    public boolean isEmpty() {
+        return size==0;
     }
 
-    /**
-     * Returns the number of items in the deque.
-     * @return
-     */
     public int size() {
         return size;
     }
 
-    /**
-     * Prints the items in the deque from first to last, separated by a space.
-     */
-    public void printDeque(){
-//        for(int i = this.nextFirst + 1; i < size; i++){
-//            System.out.print(this.arrays[i] + " ");
-//        }
-//        System.out.println();
-        boolean flag = true;
-        int first = this.nextFirst + 1;
-        int last = this.nextLast;
-
-        while (flag) {
-
-            System.out.print(this.arrays[first]);
-            first++;
-
-            if (first > this.arrays.length - 1) {
-                first = 0;
-            }
-
-            if (first == last) {
-                flag = false;
-            }
-        }
-
-        System.out.println();
-
+    public void printDeque() {
+        //nextFirst有可能指向最后一个位置
+        for (int i=(nextFirst+1)%capacity;i!=nextLast-1;i=(i+1)%capacity)
+            System.out.print(items[i]+" ");
+        System.out.print(items[nextLast-1]);
     }
 
-    private void resizeWithIncrease() {
-        //从nextFirst -> nextLast
-        T[] newArray = (T[]) new Object[this.arrays.length * 2];
-        boolean flag = true;
-        int index = 0;
-
-        while(flag){
-            //结点相邻
-            if(this.nextFirst != this.nextLast){
-                //这里还有个特殊结点,就是nextFirst在尾部结点
-                if(this.nextFirst == this.arrays.length - 1){
-                    //nextFirst走到了右边界，置于0
-                    this.nextFirst = 0;
-                }else {
-                    this.nextFirst++;
-                }
-                //先+1是因为0位留给nextFirst,因为nextFirst要是空位
-                if(this.arrays[this.nextFirst] != null){
-                    index++; //从1开始把0留给nextFirst
-                    newArray[index] = this.arrays[this.nextFirst];
-                }
-            } else {
-                flag = false;
-            }
-        }
-        this.nextFirst = 0;
-        this.nextLast = index + 1; //最后再加一是因为nextLast要是空的
-        this.arrays = newArray;
-    }
-
-    private void resizeIDecrease() {
-        if (this.arrays.length < 16) {
-            return;
-        }
-
-        if (size * 2 > this.arrays.length) {
-            return;
-        }
-
-        //空置率至少一半，缩小25%
-        int length = Double.valueOf(this.arrays.length * 0.75).intValue();
-        T[] newArray = (T[]) new Object[length];
-
-        boolean flag = true;
-        int index = 0;
-
-        while (flag) {
-            if (this.nextFirst != this.nextFirst) {
-
-                if (this.nextFirst >= this.arrays.length - 1) this.nextFirst = 0;
-                else this.nextFirst++;
-                if (this.arrays[this.nextFirst] != null) {
-                    index++;
-                    newArray[index] = this.arrays[this.nextFirst];
-                }
-            }
-            else flag =false;
-        }
-
-        this.nextFirst = 0;
-        this.nextLast = index + 1;
-        this.arrays = newArray;
-    }
-
-    /**
-     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
-     * @return
-     */
-    public T removeFirst(){
-        if(isEmpty()) {
-            return null;
-        }
-        if(this.nextFirst == this.arrays.length - 1){
-            this.nextFirst = 0;
-        } else {
-            this.nextFirst ++;
-        }
-        T t = this.arrays[this.nextFirst];
-        this.arrays[this.nextFirst] = null;
+    public T removeFirst() {
+        //当数组的内容为空的时候，才无法进行remove操作，而不是取决于nextFirst的位置。
+        if (size==0)return null;
+        nextFirst=(nextFirst+1)%capacity;
+        T temp=items[nextFirst];
+        items[nextFirst]=null;
         size--;
-        resizeIDecrease();
-        return t;
+        if (capacity>=16&&size<capacity/4)
+            resize(capacity/2);
+        return temp;
     }
 
-    /**
-     * Removes and returns the item at the end of the deque. If no such item exists, returns null.
-     * @return
-     */
-    public T removeLast(){
-        if(isEmpty())
-            return null;
-        if(this.nextLast == 0) {
-            this.nextLast = this.arrays.length - 1;
-        }
-        else {
-            this.nextLast--;
-        }
-        T t = this.arrays[this.nextLast];
-        this.arrays[this.nextLast] = null;
+    public T removeLast() {
+        if (size==0)return null;
+        nextLast=nextLast==0?capacity-1:nextLast-1;
+        T temp=items[nextLast];
+        items[nextLast]=null;
         size--;
-        resizeIDecrease();
-        return t;
+        if (capacity>=16&&size<capacity/4)
+            resize(capacity/2);
+        return temp;
     }
 
-    /**
-     *  Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
-     *  If no such item exists, returns null. Must not alter the deque!
-     * @param index
-     * @return
-     */
-    public T get(int index){
-        if(index < 0 || index > size - 1)
+    public T get(int index) {
+        if (index>=size)
             return null;
-        int n = this.nextFirst + 1 + index; //n为index位置上的元素在数组中的实际位置
-
-        if(n < this.arrays.length)
-            return this.arrays[n];
-            else return this.arrays[n - this.arrays.length];
+        return items[(nextFirst+1+index)%capacity];
     }
-
-
 }
